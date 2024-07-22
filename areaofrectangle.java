2ac7ea52cee170080e4f0019c7d79bd896d3e05b
Ananya Sharma <148464979+Ananyyasharma@users.noreply.github.com>
2024-07-22 21:32:24 +0530

    Add files via upload

---
+++ areaofrectangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+ 
+
+class areaofrectangle{
+
+    
+
+    public static void main(String args[]) {
+
+        
+
+        // Write code here
+
+        Scanner input = new Scanner(System.in);
+
+        int L = input.nextInt();
+
+        int B = input.nextInt();
+
+        int A = L * B;
+
+        System.out.println(A);
+
+        
+
+    }
+
+}
+
+
